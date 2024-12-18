b88a95939dd807527293e19c2372b62b98992473
tande21 <tande21@student.sdu.dk>
2024-12-18 09:57:45 +0100

    sentiment.py updated

--- sentiment.py
+++ sentiment.py
@@ -5,27 +5,32 @@ from helper import save_dataframe_to_csv
 
 class SentimentInfer:
     def __init__(self, model="distilbert-base-uncased-finetuned-sst-2-english"):
-        # Initialize the sentiment analysis pipeline
+        """
+        Initialize the sentiment inference class.
+        Args:
+            model (str): Name of the pre-trained model to use for sentiment analysis.
+        """
         self.sentiment_pipeline = pipeline("sentiment-analysis", model=model, truncation=True)
         self.df = pd.read_csv('COMPLETED_DF.csv')
     
     def infer(self, text):
-        # Use the pipeline to predict sentiment
+        """
+        Perform sentiment analysis on a given text.
+        Args:
+            text (str): Input text for sentiment analysis.
+        Returns:
+            result (list): List containing sentiment analysis results.
+        """
         result = self.sentiment_pipeline(text)
         return result
-    
-    
 
-
-# Example usage:
 if __name__ == "__main__":
     sentiment_infer = SentimentInfer()
     df = sentiment_infer.df
-    # text = "I love this product! It's amazing."
- #   df["Sentiment"] = df["combined_text"].apply(lambda x: sentiment_infer.infer(x))
+
     df["Sentiment"] = df["combined_text_stop"].apply(
-    lambda x: sentiment_infer.infer(str(x)) if pd.notnull(x) else None
-)
-    #print(sentiment_infer.infer(text))
+        lambda x: sentiment_infer.infer(str(x)) if pd.notnull(x) else None
+    )
+
     print(df["Sentiment"].head())
-    save_dataframe_to_csv("theis_output.csv", df)
+    save_dataframe_to_csv("complete_df_sentiment.csv", df)
