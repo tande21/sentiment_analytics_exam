3cbc2acfe4c8329cef62deee54ac75537b6e9423
Holycoffee1337 <73473256+Holycoffee1337@users.noreply.github.com>
2024-12-18 14:05:56 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,9 @@
+"""
+file: sentiment.py
+purpose: Building a classifier, that can make,
+    sentiment classifications. This is used,
+    to find the sentiment of the different post. 
+"""
 from transformers import pipeline
 import pandas as pd
 import helper
@@ -5,22 +11,10 @@ from helper import save_dataframe_to_csv
 
 class SentimentInfer:
     def __init__(self, model="distilbert-base-uncased-finetuned-sst-2-english"):
-        """
-        Initialize the sentiment inference class.
-        Args:
-            model (str): Name of the pre-trained model to use for sentiment analysis.
-        """
         self.sentiment_pipeline = pipeline("sentiment-analysis", model=model, truncation=True)
         self.df = pd.read_csv('COMPLETED_DF.csv')
     
     def infer(self, text):
-        """
-        Perform sentiment analysis on a given text.
-        Args:
-            text (str): Input text for sentiment analysis.
-        Returns:
-            result (list): List containing sentiment analysis results.
-        """
         result = self.sentiment_pipeline(text)
         return result
 
